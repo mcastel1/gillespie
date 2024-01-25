cf79dcd598d7cb5a10e5044124004a192b5e9814
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-01-25 17:29:11 +0100

    System

--- gillespie/main.cpp
+++ gillespie/main.cpp
@@ -7,8 +7,35 @@
 
 #include <iostream>
 
+#include "gsl_rng.h"
+
+using namespace std;
+
+class System{
+    
+public:
+    
+    long n_species, n_reactions;
+    vector<long> x;
+    
+    System(long, long);
+    
+
+};
+
+System::System(long n_species_in, long n_reactions_in){
+    
+    n_species = n_species_in;
+    n_reactions = n_reactions_in;
+    x.resize(n_species);
+    
+}
+
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
+    
 }
