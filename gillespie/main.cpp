233e9a1d13586bd645919fdb7fcb8b6c24e9da39
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-01-26 13:24:54 +0100

    Included runtime flags

--- gillespie/main.cpp
+++ gillespie/main.cpp
@@ -15,27 +15,67 @@ class System{
     
 public:
     
-    long n_species, n_reactions;
+    long n_species, n_reactions, seed;
     vector<long> x;
+    gsl_rng* ran;
     
-    System(long, long);
+    System(long, long, long);
+    void iterate(void);
     
 
 };
 
-System::System(long n_species_in, long n_reactions_in){
+System::System(long n_species_in, long n_reactions_in, long seed_in){
     
     n_species = n_species_in;
     n_reactions = n_reactions_in;
+    seed = seed_in;
+
     x.resize(n_species);
+    ran = gsl_rng_alloc(gsl_rng_gfsr4);
+    gsl_rng_set(ran, seed);
+
+    
+}
+
+void System::iterate(void){
+    
+    
     
 }
 
 
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    int options;
+    long S, R, s;
+    
+    
+    while ((options = getopt(argc, argv, ":S:R:s:")) != -1) {
+         
+         switch (options) {
+                 
+             case 'S':
+                 S = (long)atoi(optarg);
+                 break;
+                 
+             case 'R':
+                 R = strtod(optarg,argv);
+                 break;
+                 
+             case 's':
+                 s = strtod(optarg,argv);
+                 break;
+                 
+        
+                 
+         }
+         
+     }
+    
+    
+    
     return 0;
     
 }
