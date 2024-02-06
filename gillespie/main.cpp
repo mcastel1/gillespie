1e8f2e02163d1e24499f2a4df3f63c587fa9010f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-06 17:07:52 +0100

    Adapted to Franck model

--- gillespie/main.cpp
+++ gillespie/main.cpp
@@ -1,11 +1,16 @@
-//
-//  main.cpp
-//  gillespie
-//
-//  Created by Michele on 25/01/2024.
-//
-
 #include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <strstream>
+#include <string.h>
+#include <sstream>
+#include <algorithm>
+#include <list>
+#include <stdio.h>
+#include <time.h>  
+#include <getopt.h>
 
 #include "gsl_rng.h"
 
@@ -15,23 +20,29 @@ class System{
     
 public:
     
-    long n_species, n_reactions, seed;
-    vector<long> x;
+    long seed;
+    vector<unsigned long long int> x;
     gsl_rng* ran;
     
-    System(long, long, long);
+    System(void);
+    System(long);
     void iterate(void);
     
 
 };
 
-System::System(long n_species_in, long n_reactions_in, long seed_in){
+System::System(void){
     
-    n_species = n_species_in;
-    n_reactions = n_reactions_in;
+  
+    
+}
+
+
+System::System(long seed_in){
+    
     seed = seed_in;
 
-    x.resize(n_species);
+    x.resize(3);
     ran = gsl_rng_alloc(gsl_rng_gfsr4);
     gsl_rng_set(ran, seed);
 
@@ -46,29 +57,22 @@ void System::iterate(void){
 
 
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
     
     int options;
-    long S, R, s;
+    
+    System sys;
     
     
-    while ((options = getopt(argc, argv, ":S:R:s:")) != -1) {
+    
+    
+    while ((options = getopt(argc, argv, ":Ss:")) != -1) {
          
          switch (options) {
                  
-             case 'S':
-                 S = (long)atoi(optarg);
-                 break;
-                 
-             case 'R':
-                 R = strtod(optarg,argv);
-                 break;
-                 
              case 's':
-                 s = strtod(optarg,argv);
+                 (sys.seed) = (long)atoi(optarg);
                  break;
-                 
-        
                  
          }
          
