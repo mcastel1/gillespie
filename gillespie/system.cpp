e4c62cd60261c542163c6d9a9a7add9a9db121e9
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-07 14:19:48 +0100

    created .hpp files

---
+++ gillespie/system.cpp
@@ -0,0 +1,8 @@
+//
+//  system.cpp
+//  gillespie
+//
+//  Created by Michele on 07/02/2024.
+//
+
+#include "system.hpp"
---
+++ gillespie/system.hpp
@@ -0,0 +1,13 @@
+//
+//  system.hpp
+//  gillespie
+//
+//  Created by Michele on 07/02/2024.
+//
+
+#ifndef system_hpp
+#define system_hpp
+
+#include <stdio.h>
+
+#endif /* system_hpp */
