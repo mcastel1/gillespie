b96cee7610e0aa521c041e1120d71baaaf19a9b7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-12 17:07:56 +0100

    Created BitSet class ..

---
+++ gillespie/bitset.cpp
@@ -0,0 +1,8 @@
+//
+//  bitset.cpp
+//  gillespie
+//
+//  Created by Michele on 12/02/2024.
+//
+
+#include "bitset.hpp"
---
+++ gillespie/bitset.hpp
@@ -0,0 +1,13 @@
+//
+//  bitset.hpp
+//  gillespie
+//
+//  Created by Michele on 12/02/2024.
+//
+
+#ifndef bitset_hpp
+#define bitset_hpp
+
+#include <stdio.h>
+
+#endif /* bitset_hpp */
