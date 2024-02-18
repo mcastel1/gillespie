1588c67204af4a55f1b2455f0b0bce2adcc47f96
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-18 18:48:56 +0100

    Created Int class

---
+++ gillespie/int.cpp
@@ -0,0 +1,11 @@
+//
+//  int.cpp
+//  gillespie
+//
+//  Created by Michele on 18/02/2024.
+//
+
+#include "int.hpp"
+
+
+
---
+++ gillespie/int.hpp
@@ -0,0 +1,14 @@
+//
+//  int.hpp
+//  gillespie
+//
+//  Created by Michele on 18/02/2024.
+//
+
+class Int{
+    
+private:
+    Int(void);
+    
+    
+};
