c74cff0e9df0c7ddaa6e6cf0d271af3449f1f9be
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-19 12:11:29 +0100

    Working on d BitSet::operator <<=( ...

--- gillespie/unsigned_int.cpp
+++ gillespie/unsigned_int.cpp
@@ -90,8 +90,55 @@ inline Bits UnsignedInt::operator < (const UnsignedInt& m){
 }
 
 //I am obliged to put this method definition here, because this method needs the full declaration of the UnsignedInt class before it is declared
-//shift bit-by-bit the entries of  b[0] b[1] ... in *this by a number of positions encoded in *e and replace the remaining entries b[] by all zeros
+//shift bit-by-bit the entries of  b[51] b[50] ... in *this by a number of positions to the right (>>) encoded in *e and replace the remaining entries b[] by all zeros
 inline void BitSet::operator >>=(UnsignedInt* e){
+    
+    unsigned int n;
+    int m;
+    Bits zero;
+    
+    zero.SetAll(false);
+    
+    
+    for(n=0; n<(e->GetSize()); n++){
+        //shift by 2^n positions according to e[n]
+        
+        //run through the components of this->b and shift them
+        
+        //in this first loop, I run over the first chunk of entries of b: m = 0, ..., b.size() - 2^n-1 and I replace the m-th component of b with the m+2^n-th compoennt if e[n]=true, and do nothing otherwise
+        for(m=0; m<(this->GetSize())-gsl_pow_int(2, n); m++){
+
+            b[m].Replace(
+                         //the element # m+2^n in b
+                         &(b[m+two_pow(n)]),
+                         //the element #n in e
+                         &((e->b)[n])
+                         );
+        
+            
+        }
+        
+        //in this loop, I run over the second chunk of entries of b: m =b.size() - 2^n , ..., b.size(). I can no longer replace b[m] with b[m+2^n] as in the loop above, becuase b[m+2^n] does not exist in b -> I replace it with zero. Note that if 2^n > b.size(), this second loop must cover the entire vector b -> I set as starting value of m  max(((int)(this->GetSize()))-((int)gsl_pow_int(2, n)), 0)
+        for(m = max(((int)(this->GetSize()))-((int)gsl_pow_int(2, n)), 0); (m<(this->GetSize())); m++){
+                     
+            b[m].Replace(
+                         //a Bits filled with zeros
+                         &zero,
+                         //the element #n in e
+                         &((e->b)[n])
+                         );
+        
+            
+        }
+        
+    }
+    
+    
+}
+
+
+//shift bit-by-bit the entries of  b[51] b[50] ... in *this by a number of positions to the left (<<) encoded in *e and replace the remaining entries b[] by all zeros
+inline void BitSet::operator <<=(UnsignedInt* e){
     
     unsigned int n;
     int m;
