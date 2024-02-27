252a462725d8e8f28de8a44130e87455639b418b
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-27 21:06:16 +0100

    Initialized values of c

--- gillespie/system.cpp
+++ gillespie/system.cpp
@@ -13,16 +13,22 @@ System::System(void){
 
 System::System(unsigned int N_in, unsigned int seed_in){
     
-    unsigned int i;
+    unsigned int i, s;
     
     ran = gsl_rng_alloc(gsl_rng_gfsr4);
 
     N = N_in;
     seed = seed_in;
 
-    for(x.clear(), i=0; i<3; ++i){
+    for(x.clear(), c.clear(), i=0; i<3; ++i){
+        
         x.push_back(UnsignedInt(N));
+        
         c.push_back(UnsignedInt(n_bits_N));
+        for(s=0; s<n_bits; s++){
+            c[i].Set(s, gsl_rng_uniform_int(ran, pow(2, n_bits_N)));
+        }
+        
     }
     
     gsl_rng_set(ran, seed);
