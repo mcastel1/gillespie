455cc84528073d64186926afe1d0e4b51674c6c7
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-02-28 10:47:54 +0100

    Added comment

--- gillespie/system.hpp
+++ gillespie/system.hpp
@@ -11,7 +11,7 @@ class System{
 public:
     
     unsigned long long int seed, /*the total number of molecules in the system*/N;
-    vector<UnsignedInt> /*the vector containing the numbers of molecules: x[i] is a vector. { x[i].b[0], x[i].b[1], x[i].b[2], ... } is the expression in base 2 of the number of molecules of species i*/x, /*the reaction rates: a[j](x) = c[j] * f(x), where c[j] is independent of x*/c, /*the quantities a_j in Gillespie algorithm*/a;
+    vector<UnsignedInt> /*the vector containing the numbers of molecules: x[i] is a vector. { x[i].b[0], x[i].b[1], x[i].b[2], ... } is the expression in base 2 of the number of molecules of species i, x[0] -> L, x[1] -> D, x[2] -> A*/x, /*the reaction rates: a[j](x) = c[j] * f(x), where c[j] is independent of x*/c, /*the quantities a_j in Gillespie algorithm, a[0] corresponds to the resction A+D -> 2D, a[1] to the reaction A+L -> 2L, a[2] to the reaction D+L->2A*/a;
     gsl_rng* ran;
     
     System(void);
