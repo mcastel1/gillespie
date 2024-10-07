6a0e51e3c2afca61958db863a255617ae9fe3814
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-10-07 14:57:10 +0200

    Created lib.hpp

---
+++ include/lib.hpp
@@ -0,0 +1,21 @@
+//
+//  lib.h
+//  gillespie
+//
+//  Created by Michele Castellana on 07/10/2024.
+//
+
+#ifndef lib_hpp
+#define lib_hpp
+
+#include <iostream>
+
+
+using namespace std;
+
+unsigned long long int two_pow(unsigned long long int);
+unsigned int inline bits(unsigned long long int);
+void bitwise_print_double(const double& x);
+void GetMantissaFromDouble(vector<bool>* result, double x);
+
+#endif /* lib_h */
