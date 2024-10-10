2f7762ea83f43b9fab82bfd36644994d4beaae97
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-10-10 13:14:29 +0200

    Update bitset.cpp

--- src/bitset.cpp
+++ src/bitset.cpp
@@ -793,7 +793,7 @@ void BitSet::Multiply(UnsignedInt* multiplicand, UnsignedInt* result){
         for(p=0, carry.Clear(); p<GetSize(); p++){
             
             u.Set(((*multiplicand)[s]) & (b[p]));
-            (t.Get()) = ((((result->b)[p+s]).Get()) ^ (u.Get()) ^ (carry.Get()));
+            t.Set(((result->b)[p+s]) ^ u ^ carry);
             
             (carry.Get()) = ((u.Get()) & ((((result->b)[p+s]).Get()) | (carry.Get()))) | ((((result->b)[p+s]).Get()) & (carry.Get()));
             ((result->b)[p+s]).Get() = (t.Get());
