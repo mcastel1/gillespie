7da03655730e71608ca7c712092e3065d89cbf9f
mcastel1 <40599056+mcastel1@users.noreply.github.com>
2024-10-26 14:29:23 +0200

    Create test_performance.sh

---
+++ test_performance.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+clear; clear;
+
+./main.o -N 4 -S 5 -s 0 -o /Users/michelecastellana/Desktop
+./main.o -N 8 -S 5 -s 0 -o /Users/michelecastellana/Desktop
+./main.o -N 16 -S 5 -s 0 -o /Users/michelecastellana/Desktop
+./main.o -N 32 -S 5 -s 0 -o /Users/michelecastellana/Desktop
+./main.o -N 64 -S 5 -s 0 -o /Users/michelecastellana/Desktop
+./main.o -N 128 -S 5 -s 0 -o /Users/michelecastellana/Desktop
+./main.o -N 256 -S 5 -s 0 -o /Users/michelecastellana/Desktop
